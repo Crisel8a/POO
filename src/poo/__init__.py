9bc1941ab790bc031100c465505884ad78bd8ccf
Crisel <Crisel>
2026-09-11 20:06:34 -0600

    06

--- src/poo/__init__.py
+++ src/poo/__init__.py
@@ -1,22 +1,42 @@
 class Libro:
-    def __init__(self, titulo, autor, paginas, dospos=None):
+    def __init__(self, titulo, autor, isbn, disponible=True, count=0):
         self.titulo = titulo
         self.autor = autor
-        self.paginas = paginas
-        self.dospos = dospos
+        self.isbn = isbn
+        self.disponible = disponible
+        self.count = count
+
+    def __str__(self):
+        return f"{self.titulo} por {self.autor} disponible: {self.disponible}"
+
+    def prestar(self):
+        if self.disponible:
+            self.disponible = False
+            self.count += 1
+        return f"'{self.titulo}' prestado exitosamente"
+
+    def es_popular(self):
+        if self.count >= 5:
+            return f"'{self.titulo}' es un libro popular"
+        else:
+            return f"'{self.titulo}' no es un libro popular"
+
+    def devolver(self):
+        self.disponible = True
+        return f"'{self.titulo}' devuelto y disponible nuevamente"
 
 
 mi_libro = Libro(
-    "El Principito", "Antoine de Saint-Exupéry", 96, dospos="Edición especial"
+    "100 Años de Soledad", "Gabriel Garcia Marquez", "9781644734728", True, 5
 )
-otro_libro = Libro("1984", "George Orwell", 328, dospos="Edición de bolsillo")
-un_libro = Libro(
-    "Cien Años de Soledad", "Gabriel García Márquez", 417, dospos="Edición de lujo"
-)
+otro_libro = Libro("El Principito", "Saint-Exupéry", "9781644731234728", True, 3)
+un_libro = Libro("El Alquimista", "Paulo Coelho", "9781644731234728", True, 12)
 
 catalogo = [mi_libro, otro_libro, un_libro]
 
 for libro in catalogo:
-    print(
-        f"Título: {libro.titulo}, Autor: {libro.autor}, Páginas: {libro.paginas}, Dospos: {libro.dospos}"
-    )
+    print(libro)
+    print(libro.prestar())
+    print(libro.es_popular())
+    print(libro.devolver())
+    print()
