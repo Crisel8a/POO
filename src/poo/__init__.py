7f5a912d5d99e3698f18935b571c10891f4e0999
Crisel <Crisel>
2026-09-11 19:22:00 -0600

    02

--- src/poo/__init__.py
+++ src/poo/__init__.py
@@ -1,16 +1,22 @@
 class Libro:
-    def __init__(self, titulo, autor, paginas):
+    def __init__(self, titulo, autor, paginas, dospos=None):
         self.titulo = titulo
         self.autor = autor
         self.paginas = paginas
+        self.dospos = dospos
 
 
-mi_libro = Libro("El Principito", "Antoine de Saint-Exupéry", 96)
-otro_libro = Libro("1984", "George Orwell", 328)
-
-print(
-    f"Mi libro favorito es '{mi_libro.titulo}' de {mi_libro.autor}, que tiene {mi_libro.paginas} páginas."
+mi_libro = Libro(
+    "El Principito", "Antoine de Saint-Exupéry", 96, dospos="Edición especial"
 )
-print(
-    f"Otro libro es '{otro_libro.titulo}' de {otro_libro.autor}, que tiene {otro_libro.paginas} páginas."
+otro_libro = Libro("1984", "George Orwell", 328, dospos="Edición de bolsillo")
+un_libro = Libro(
+    "Cien Años de Soledad", "Gabriel García Márquez", 417, dospos="Edición de lujo"
 )
+
+catalogo = [mi_libro, otro_libro, un_libro]
+
+for libro in catalogo:
+    print(
+        f"Título: {libro.titulo}, Autor: {libro.autor}, Páginas: {libro.paginas}, Dospos: {libro.dospos}"
+    )
